f2ffbeea60ffa2a1ecf808b48ffa5ef3a5b03afa
brokencranium <vikas.vennavali@gmail.com>
2017-07-04 22:12:19 -0700

    Added Gradient boosting

--- Pick_GB.py
+++ Pick_GB.py
@@ -2,6 +2,7 @@ import numpy as np
 from pandas import read_csv
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn import ensemble
 
 input_data = read_csv("/home/vicky/Documents/it/AI/data/picks_2016.csv")
@@ -13,10 +14,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random
 
 params = {'n_estimators': 500, 'max_depth': 6, 'min_samples_split': 2,
           'learning_rate': 0.01, 'loss': 'ls'}
-gb_mod = ensemble.GradientBoostingRegressor(**params)
+gb_en_mod = ensemble.GradientBoostingRegressor(**params)
+
+gb_mod = GradientBoostingRegressor(loss='quantile', alpha=0.5, n_estimators=250, max_depth=6, learning_rate=.1,
+                                   min_samples_leaf=3, min_samples_split=3)
+
+gb_em_fit = gb_en_mod.fit(X_train, y_train['PPS'])
+gb_em_pred = gb_em_fit.predict(X_test)
+rms_gb_em = np.sqrt(mean_squared_error(y_test, gb_em_pred))
 
 gb_fit = gb_mod.fit(X_train, y_train['PPS'])
 gb_pred = gb_fit.predict(X_test)
 rms_gb = np.sqrt(mean_squared_error(y_test, gb_pred))
-print(rms_gb)
-print(gb_pred)
+
+print("Ensemble Gradient Boosting", rms_gb_em)
+print("Gradient boosting", rms_gb)
